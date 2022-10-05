a36fc2e56eff628c5e44f1106fa43dfa4997b07c
Jason <jason.morris@bugsnag.com>
2022-10-05 12:04:45 +0100

    feat(test): added mazerunner as a BuildKite step

--- features/fixtures/mazeracer/app/src/main/java/com/bugsnag/mazeracer/MainActivity.kt
+++ features/fixtures/mazeracer/app/src/main/java/com/bugsnag/mazeracer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.bugsnag.mazeracer
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.Window
