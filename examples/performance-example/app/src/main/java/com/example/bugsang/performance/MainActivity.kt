946a94913e3050a447ac53bf7f2cdf0cb97167b7
Jason <jason.morris@bugsnag.com>
2022-11-30 08:13:26 +0000

    feat(example): added a new example app

---
+++ examples/performance-example/app/src/main/java/com/example/bugsang/performance/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.bugsang.performance
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val network = ExampleNetworkCalls(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.network_request).setOnClickListener {
+            network.runRequest()
+        }
+    }
+}
