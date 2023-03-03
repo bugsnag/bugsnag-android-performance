f816f8dfbd833db47b8471ecba056a40735add6f
Jason <jason.morris@bugsnag.com>
2023-03-03 08:41:18 +0000

    refactor(stack): removed JvmSynthetic from SpanContext.Storage.currentStack

--- bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/SpanContextExtensions.kt
+++ bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/SpanContextExtensions.kt
@@ -5,11 +5,8 @@ import com.bugsnag.android.performance.SpanContext
 /**
  * Allows getting/setting the Span Context stack for the current thread.
  *
- * This is utilized by the BugSnag Performance Coroutines plugin and is intended for
- * internal use only.
+ * This is intended for internal use only.
  */
-@get:JvmSynthetic
-@set:JvmSynthetic
 var SpanContext.Storage.currentStack
     get() = contextStack
     set(value) = setContextStackUnsafe(value)
