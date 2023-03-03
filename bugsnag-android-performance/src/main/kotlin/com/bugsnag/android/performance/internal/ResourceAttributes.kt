be9daa7dfc5662bc48c5f2329540d619a813738c
Jason <jason.morris@bugsnag.com>
2023-03-03 17:18:48 +0000

    refactor(config): separated out `ImmutableConfig` for internal use once `PerformanceConfiguration` has been validated and merged with defaults

--- bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/ResourceAttributes.kt
+++ bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/ResourceAttributes.kt
@@ -1,14 +1,11 @@
 package com.bugsnag.android.performance.internal
 
-import android.content.Context
 import android.os.Build
 import com.bugsnag.android.performance.Attributes
 import com.bugsnag.android.performance.BugsnagPerformance
-import com.bugsnag.android.performance.PerformanceConfiguration
 
-internal fun createResourceAttributes(configuration: PerformanceConfiguration): Attributes {
+internal fun createResourceAttributes(configuration: ImmutableConfig): Attributes {
     val resourceAttributes = Attributes()
-    val context = configuration.context
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
         resourceAttributes["host.arch"] = abiToArchitecture(Build.SUPPORTED_ABIS.firstOrNull())
@@ -23,14 +20,11 @@ internal fun createResourceAttributes(configuration: PerformanceConfiguration): 
 
     resourceAttributes["device.model.identifier"] = Build.MODEL
     resourceAttributes["device.manufacturer"] = Build.MANUFACTURER
-    resourceAttributes["deployment.environment"] =
-        configuration.releaseStage ?: context.releaseStage
+    resourceAttributes["deployment.environment"] = configuration.releaseStage
 
-    configuration.versionCodeFor(context)?.let { versionCode ->
-        resourceAttributes["bugsnag.app.version_code"] = versionCode.toString()
-    }
+    resourceAttributes["bugsnag.app.version_code"] = configuration.versionCode.toString()
 
-    resourceAttributes["service.name"] = configuration.context.packageName
+    resourceAttributes["service.name"] = configuration.packageName
     resourceAttributes["telemetry.sdk.name"] = "bugsnag.performance.android"
     resourceAttributes["telemetry.sdk.version"] = BugsnagPerformance.VERSION
 
@@ -43,19 +37,4 @@ private fun abiToArchitecture(abi: String?): String? = when (abi?.lowercase()) {
     "armeabi-v7a" -> "arm32"
     "x86" -> "x86"
     else -> null
-}
-
-private fun PerformanceConfiguration.versionCodeFor(context: Context): Long? {
-    if (versionCode != null) return versionCode
-    return try {
-        val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            packageInfo?.longVersionCode
-        } else {
-            @Suppress("DEPRECATION")
-            packageInfo?.versionCode?.toLong()
-        }
-    } catch (ex: RuntimeException) {
-        null
-    }
 }
