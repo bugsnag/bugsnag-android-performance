63a8a38b5243cb88b796dee25355ae95a5bfbaa6
Jason <jason.morris@bugsnag.com>
2023-03-15 14:35:05 +0000

    chore: removed unused `SpanChain` functions and tests

--- bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/SpanChain.kt
+++ bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/SpanChain.kt
@@ -9,99 +9,6 @@ package com.bugsnag.android.performance.internal
 internal typealias SpanChain = SpanImpl
 
 /**
- * *Warning O(n) operation*
- *
- * Returns the number of `SpanImpl` objects in this `SpanChain`, by counting each  link in the chain.
- * This should only be called on `SpanChain`s that are stable.
- *
- * @see toList
- */
-internal val SpanChain.size: Int
-    get() {
-        var count = 0
-        var span: SpanImpl? = this
-
-        while (span != null) {
-            count++
-            span = span.next
-        }
-
-        return count
-    }
-
-/**
- * Find the head of this `SpanChain`, traversing up the chain until a `SpanImpl` with no "previous"
- * node is found. This method should only be invoked on stable `SpanChain`s, that is: those
- * only accessible be a single thread.
- */
-internal fun SpanChain.tail(): SpanImpl {
-    var span = this
-
-    // this awkward little dance of keeping `span.next` local avoids extra null enforcement, and
-    // slightly side-steps some possible races
-    var previous = span.next
-
-    while (previous != null) {
-        span = previous
-        previous = span.next
-    }
-
-    return span
-}
-
-internal inline fun SpanChain.forEach(action: (SpanImpl) -> Unit) {
-    var span: SpanImpl? = this
-
-    while (span != null) {
-        action(span)
-        span = span.next
-    }
-}
-
-internal inline fun SpanChain.find(predicate: (SpanImpl) -> Boolean): SpanImpl? {
-    var span: SpanImpl? = this
-    while (span != null) {
-        if (predicate(span)) {
-            return span
-        }
-
-        span = span.next
-    }
-
-    return null
-}
-
-/**
- * Filter this span chain (starting at this `SpanChain`) using the given [predicate] and return the
- * new "tail" of the filtered chain. Unlike normal `filter` operations this is *destructive* once
- * called the `SpanImpl`s in the chain should *only* be accessed by the returned value.
- *
- * @see toList
- */
-internal inline fun SpanChain.filter(predicate: (SpanImpl) -> Boolean): SpanImpl? {
-    val root = find(predicate) ?: return null
-
-    var span: SpanImpl? = root
-    var tail = root
-    while (span != null) {
-        if (!predicate(span)) {
-            tail.next = span.next
-            span.next?.let { tail = it }
-        }
-
-        span = span.next
-    }
-
-    return root
-}
-
-internal fun SpanChain.toList(): List<SpanImpl> {
-    val list = ArrayList<SpanImpl>()
-    forEach(list::add)
-    return list
-}
-
-/**
  * Unrolls this [SpanChain] into the given [output] collection. This is *destructive* and the
  * [SpanChain] links will be broken as part of this process (turning each link back into a "normal"
  * [SpanImpl]).
@@ -116,7 +23,3 @@ internal fun <C : MutableCollection<SpanImpl>> SpanChain.unlinkTo(output: C): C 
     }
     return output
 }
-
-internal fun SpanChain?.isNotEmpty() = this != null
-
-internal fun SpanChain?.isEmpty() = this == null
--- bugsnag-android-performance/src/test/java/com/bugsnag/android/performance/internal/SpanChainTest.kt
+++ bugsnag-android-performance/src/test/java/com/bugsnag/android/performance/internal/SpanChainTest.kt
@@ -3,11 +3,8 @@ package com.bugsnag.android.performance.internal
 import com.bugsnag.android.performance.test.TestSpanFactory
 import com.bugsnag.android.performance.test.testSpanProcessor
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
-import org.junit.Assert.assertSame
-import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -17,84 +14,6 @@ class SpanChainTest {
     @Before
     fun setupDependencies() {
         spanFactory = TestSpanFactory()
-    }
-
-    @Test
-    fun filterFirstItem() {
-        val span1 = spanFactory.newSpan(spanId = 1L, processor = testSpanProcessor)
-        val span2 = spanFactory.newSpan(spanId = 2L, processor = testSpanProcessor)
-
-        span1.next = span2
-
-        val filteredChain = span1.filter { it.spanId != 1L }
-
-        assertSame(span2, filteredChain)
-        assertNull(filteredChain!!.next)
-    }
-
-    @Test
-    fun filterItems() {
-        val spans = spanFactory.newSpans(10, testSpanProcessor)
-        val root = spans.toSpanChain()
-
-        val filtered = root.filter { it.spanId % 2 == 0L }!!.toList()
-        assertEquals(5, filtered.size)
-        // SpanFactory starts counting at 1
-        assertSame(spans[1], filtered[0]) // id == 2
-        assertSame(spans[3], filtered[1]) // id == 4
-        assertSame(spans[5], filtered[2]) // id == 6
-        assertSame(spans[7], filtered[3]) // id == 8
-        assertSame(spans[9], filtered[4]) // id == 10
-    }
-
-    @Test
-    fun tail() {
-        val spans = spanFactory.newSpans(10, testSpanProcessor)
-        val root = spans.toSpanChain()
-
-        assertSame(spans.last(), root.tail())
-    }
-
-    @Test
-    fun filterAllItems() {
-        val spans = spanFactory.newSpans(10, testSpanProcessor)
-        val root = spans.toSpanChain()
-
-        val filtered = root.filter { it.spanId == -1L }
-        assertNull(filtered)
-    }
-
-    @Test
-    fun filterNothing() {
-        val spans = spanFactory.newSpans(10, testSpanProcessor)
-        val root = spans.toSpanChain()
-
-        val filtered = root.filter { it.spanId != -1L }!!.toList()
-        assertEquals(spans, filtered)
-    }
-
-    @Test
-    fun size() {
-        val spanList = spanFactory.newSpans(25, testSpanProcessor)
-        var chain: SpanChain? = spanList.toSpanChain()
-
-        // assert the chain size is the same for each link in the chain
-        repeat(spanList.size) { index ->
-            assertEquals(spanList.size - index, chain?.size)
-            chain = chain?.next
-        }
-    }
-
-    @Test
-    fun emptyChecks() {
-        val nullSpan: SpanChain? = null
-        val notNull: SpanChain = spanFactory.newSpan(processor = testSpanProcessor)
-
-        assertTrue(nullSpan.isEmpty())
-        assertTrue(notNull.isNotEmpty())
-
-        assertFalse(nullSpan.isNotEmpty())
-        assertFalse(notNull.isEmpty())
     }
 
     @Test
