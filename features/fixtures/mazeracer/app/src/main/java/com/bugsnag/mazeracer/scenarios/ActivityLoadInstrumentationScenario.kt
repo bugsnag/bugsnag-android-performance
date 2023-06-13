5cdd308bdd19a45bef8318a05bd7b2789fbee0fa
lemnik <lemnik@lemnik.com>
2023-06-13 15:34:14 +0100

    test(viewLoad): force MazeRunner to background to flush instead of relying on span counts or timeouts

--- features/fixtures/mazeracer/app/src/main/java/com/bugsnag/mazeracer/scenarios/ActivityLoadInstrumentationScenario.kt
+++ features/fixtures/mazeracer/app/src/main/java/com/bugsnag/mazeracer/scenarios/ActivityLoadInstrumentationScenario.kt
@@ -1,23 +1,16 @@
 package com.bugsnag.mazeracer.scenarios
 
-import android.os.Build
 import com.bugsnag.android.performance.AutoInstrument
 import com.bugsnag.android.performance.BugsnagPerformance
 import com.bugsnag.android.performance.PerformanceConfiguration
-import com.bugsnag.android.performance.internal.InternalDebug
 import com.bugsnag.mazeracer.ActivityViewLoadActivity
 import com.bugsnag.mazeracer.Scenario
 
 class ActivityLoadInstrumentationScenario(
     config: PerformanceConfiguration,
-    scenarioMetadata: String
+    scenarioMetadata: String,
 ) : Scenario(config, scenarioMetadata) {
     init {
-        InternalDebug.spanBatchSizeSendTriggerPoint = when {
-            Build.VERSION.SDK_INT < Build.VERSION_CODES.Q -> 2
-            else -> 5
-        }
-
         config.autoInstrumentActivities =
             scenarioMetadata.takeIf { it.isNotBlank() }
             ?.let { AutoInstrument.valueOf(it) }
@@ -26,8 +19,11 @@ class ActivityLoadInstrumentationScenario(
 
     override fun startScenario() {
         BugsnagPerformance.start(config)
-        context.startActivity(
-            ActivityViewLoadActivity.intent(context, config.autoInstrumentActivities)
+        startActivityAndFinish(
+            ActivityViewLoadActivity.intent(
+                context,
+                config.autoInstrumentActivities,
+            ),
         )
     }
 }
