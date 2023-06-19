5913be6ae111fd5b9f665d3cf04222a32c9f279c
lemnik <lemnik@lemnik.com>
2023-06-19 11:31:22 +0100

    chore(v0.1.6): fix the version number for the v0.1.6 release

--- bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/BugsnagPerformance.kt
+++ bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/BugsnagPerformance.kt
@@ -32,7 +32,7 @@ import java.net.URL
  * @see [start]
  */
 object BugsnagPerformance {
-    const val VERSION: String = "1.6.0"
+    const val VERSION: String = "0.1.6"
 
     internal val tracer = Tracer()
 
