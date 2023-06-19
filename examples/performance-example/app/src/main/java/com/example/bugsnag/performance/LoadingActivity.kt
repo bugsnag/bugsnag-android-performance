b2110147410d4e38cbd4071e6ba223bf4d685915
lemnik <lemnik@lemnik.com>
2023-06-19 17:13:23 +0100

    chore(example): update the example app to make the LoadingActivity clearer

--- examples/performance-example/app/src/main/java/com/example/bugsnag/performance/LoadingActivity.kt
+++ examples/performance-example/app/src/main/java/com/example/bugsnag/performance/LoadingActivity.kt
@@ -1,14 +1,29 @@
 package com.example.bugsnag.performance
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import com.bugsnag.android.performance.BugsnagPerformance
 
 class LoadingActivity : AppCompatActivity() {
+
+    private lateinit var close: Button
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        close = findViewById(R.id.close)
+        close.setOnClickListener {
+            finish()
+        }
+
+        progressBar = findViewById(R.id.progress_bar)
 
         Thread.sleep(200L)
     }
@@ -19,9 +34,18 @@ class LoadingActivity : AppCompatActivity() {
     }
 
     override fun onResume() {
+        val secondaryLoadSpan = BugsnagPerformance.startSpan("SecondaryLoad")
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                progressBar.visibility = View.GONE
+                close.visibility = View.VISIBLE
+
+                secondaryLoadSpan.end()
+            },
+            500L,
+        )
+
         super.onResume()
-        Handler(Looper.getMainLooper()).post {
-            finish()
-        }
     }
 }
