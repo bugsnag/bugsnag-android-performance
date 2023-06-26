c9eeb0287ff6982338f75e71b5e3ef6cb9d579ad
Jason <jason.morris@bugsnag.com>
2023-06-26 15:52:08 +0100

    v0.1.8

--- bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/BugsnagPerformance.kt
+++ bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/BugsnagPerformance.kt
@@ -32,7 +32,7 @@ import java.net.URL
  * @see [start]
  */
 object BugsnagPerformance {
-    const val VERSION: String = "0.1.7"
+    const val VERSION: String = "0.1.8"
 
     internal val tracer = Tracer()
 
