ef4b4ef7c7e5cfe9eb4752f5fbeb03db004a5784
YYChen01988 <Chen.yingying.01988@gmail.com>
2024-06-27 15:09:35 +0100

    feat(Compose) bump Java version

--- bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/AttributesJson.kt
+++ bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/AttributesJson.kt
@@ -4,6 +4,7 @@ package com.bugsnag.android.performance.internal
 
 import android.util.JsonWriter
 import com.bugsnag.android.performance.Attributes
+import kotlin.collections.forEach
 
 internal fun JsonWriter.value(attributes: Attributes): JsonWriter {
     return array {
