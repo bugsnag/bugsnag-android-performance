7b5cfe562a6991c641441d817a0bf3b3d8bc18ea
YYChen01988 <Chen.yingying.01988@gmail.com>
2024-08-12 10:31:43 +0100

    feat(span) SpanEndCallback logic end to end test

--- bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/SpanImpl.kt
+++ bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/SpanImpl.kt
@@ -83,7 +83,6 @@ public class SpanImpl internal constructor(
             processor.onEnd(this)
             NotifierIntegration.onSpanEnded(this)
             if (makeContext) SpanContext.detach(this)
-            isEnded = true
         }
     }
 
@@ -95,11 +94,10 @@ public class SpanImpl internal constructor(
         if (endTime.compareAndSet(NO_END_TIME, DISCARDED)) {
             NotifierIntegration.onSpanEnded(this)
             if (makeContext) SpanContext.detach(this)
-            isEnded = true
         }
     }
 
-    override fun end(): Unit =  end(SystemClock.elapsedRealtimeNanos())
+    override fun end(): Unit = end(SystemClock.elapsedRealtimeNanos())
 
     override fun isEnded(): Boolean = endTime.get() != NO_END_TIME
 
