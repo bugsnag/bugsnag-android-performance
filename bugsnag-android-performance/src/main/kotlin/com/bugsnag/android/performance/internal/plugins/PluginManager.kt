983770721607d5298b6201c199b75363cf64f069
jason <lemnik@users.noreply.github.com>
2025-05-23 08:41:38 +0100

    refactor(config): cleanup in the plugin configuration logic, merging the PluginContextImpl objects into a single context during install

--- bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/plugins/PluginManager.kt
+++ bugsnag-android-performance/src/main/kotlin/com/bugsnag/android/performance/internal/plugins/PluginManager.kt
@@ -7,26 +7,38 @@ import com.bugsnag.android.performance.Plugin
 internal class PluginManager(
     private val plugins: List<Plugin>,
 ) {
-    private val installedPlugins = LinkedHashMap<Plugin, PluginContextImpl>()
+    private val installedPlugins = ArrayList<Plugin>()
 
-    val installedPluginContexts: Collection<PluginContextImpl> get() = installedPlugins.values
+    /**
+     * The complete context after all plugins have been installed.
+     * This is used to retrieve the final configuration. This is only valid after [installPlugins]
+     * has returned.
+     */
+    var completeContext: PluginContextImpl? = null
+        private set
 
     fun installPlugins(config: PerformanceConfiguration) {
+        val mergedContexts = PluginContextImpl(config)
+
         for (plugin in plugins) {
             if (plugin !in installedPlugins) {
                 try {
                     val pluginContext = PluginContextImpl(config)
                     plugin.install(pluginContext)
-                    installedPlugins[plugin] = pluginContext
+
+                    mergedContexts.mergeFrom(pluginContext)
+                    installedPlugins.add(plugin)
                 } catch (failure: Exception) {
                     Logger.w("Plugin ${plugin::class.java.name} failed to install", failure)
                 }
             }
         }
+
+        completeContext = mergedContexts
     }
 
     fun startPlugins() {
-        installedPlugins.keys.forEach { plugin ->
+        installedPlugins.forEach { plugin ->
             try {
                 plugin.start()
             } catch (failure: Exception) {
