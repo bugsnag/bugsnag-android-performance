a4abf915b36fb09b29cbf2385bdade16189d452a
YingYing Chen <40571804+YYChen01988@users.noreply.github.com>
2025-09-03 10:18:56 +0100

    Edge to edge view in example app (#436)
    * feat(example app) edge-to-edge compatible
    
    * feat(example app) edge-to-edge compatible

--- examples/performance-example/app/src/main/java/com/example/bugsnag/performance/LoadingActivity.kt
+++ examples/performance-example/app/src/main/java/com/example/bugsnag/performance/LoadingActivity.kt
@@ -3,6 +3,7 @@ package com.example.bugsnag.performance
 import android.app.Activity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -25,6 +26,7 @@ import kotlinx.coroutines.delay
 class LoadingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             MeasuredComposable(name = "LoadingActivity") {
--- examples/performance-example/app/src/main/java/com/example/bugsnag/performance/MainActivity.kt
+++ examples/performance-example/app/src/main/java/com/example/bugsnag/performance/MainActivity.kt
@@ -3,10 +3,20 @@ package com.example.bugsnag.performance
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +26,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import com.bugsnag.android.performance.BugsnagPerformance
 import com.bugsnag.android.performance.SpanOptions
 import com.bugsnag.android.performance.compose.MeasuredComposable
@@ -25,11 +37,27 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private val network = ExampleNetworkCalls(this)
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             MeasuredComposable(name = "MainActivity") {
-                AllButtons(network = network)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = colorResource(R.color.purple_500),
+                                titleContentColor = colorResource(R.color.white),
+                            ),
+                            title = {
+                                Text(text = stringResource(R.string.app_name))
+                            },
+                        )
+                    },
+                ) { contentPadding ->
+                    AllButtons(network = network, contentPadding)
+                }
             }
         }
     }
@@ -38,12 +66,17 @@ class MainActivity : AppCompatActivity() {
 private var frameIndex = 0
 
 @Composable
-fun AllButtons(network: ExampleNetworkCalls) {
+fun AllButtons(network: ExampleNetworkCalls, contentPadding: PaddingValues) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
     var renderingSlowFrames by remember { mutableStateOf(false) }
+    val scrollStateVertical = rememberScrollState()
 
-    Column {
+    Column(
+        modifier = Modifier
+            .padding(contentPadding)
+            .verticalScroll(scrollStateVertical),
+    ) {
         DemoAction(label = "Network Request") {
             network.runRequest()
         }
@@ -92,7 +125,6 @@ fun AllButtons(network: ExampleNetworkCalls) {
                             // simulate a frozen frame
                             Thread.sleep(800L)
                         }
-
                         drawContent()
                     },
             )
