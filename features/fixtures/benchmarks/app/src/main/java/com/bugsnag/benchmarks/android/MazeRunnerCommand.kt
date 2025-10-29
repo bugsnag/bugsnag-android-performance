8f470ca5e9d107d84167b701fa5f059867d89f50
Steve Kirkland <steve.kirkland@smartbear.com>
2025-10-29 15:22:24 +0000

    Move benchmarks tests to idem-command endpoint

--- features/fixtures/benchmarks/app/src/main/java/com/bugsnag/benchmarks/android/MazeRunnerCommand.kt
+++ features/fixtures/benchmarks/app/src/main/java/com/bugsnag/benchmarks/android/MazeRunnerCommand.kt
@@ -7,8 +7,7 @@ value class MazeRunnerCommand(val json: JSONObject) {
     val id: String? get() = json.optString("uuid").takeIf { it.isNotBlank() }
     val action: String? get() = json.optString("action").takeIf { it.isNotBlank() }
     val isNoOp: Boolean get() = action == null || action == "noop"
-
+    val isResetUuid: Boolean get() = action == "reset_uuid"
     inline operator fun <reified R> get(key: String): R? = json.opt(key) as? R
-
     override fun toString(): String = json.toString()
 }
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,6 @@
 BeforeAll do
   Maze.config.receive_no_requests_wait = 10
-  Maze.config.receive_requests_wait = 60
+  Maze.config.receive_requests_wait = 120
 end
 
 Maze.hooks.before_all do
