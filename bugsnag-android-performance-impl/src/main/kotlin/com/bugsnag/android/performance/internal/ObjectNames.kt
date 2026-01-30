d76c2f2ae24d0805247bda48b560352a7d2f302a
YingYing Chen <40571804+YYChen01988@users.noreply.github.com>
2026-01-30 15:06:52 +0000

    feat(ObjectNames) added cache to save annotation class name (#498)

--- bugsnag-android-performance-impl/src/main/kotlin/com/bugsnag/android/performance/internal/ObjectNames.kt
+++ bugsnag-android-performance-impl/src/main/kotlin/com/bugsnag/android/performance/internal/ObjectNames.kt
@@ -1,11 +1,25 @@
 package com.bugsnag.android.performance.internal
 
 import com.bugsnag.android.performance.BugsnagName
+import java.util.concurrent.ConcurrentHashMap
 
 public class ObjectNames {
+    private val cache = ConcurrentHashMap<Class<*>, String>()
+
     public operator fun get(obj: Any): String {
         val objClass = obj::class.java
+
+        var objName = cache[objClass]
+        if (objName == null) {
+            val newName = calculateName(objClass)
+            val racedName = cache.putIfAbsent(objClass, newName)
+            objName = racedName ?: newName
+        }
+        return objName
+    }
+
+    private fun calculateName(objClass: Class<out Any>): String {
         val annotation = objClass.getAnnotation(BugsnagName::class.java)
-        return annotation?.value ?: obj::class.java.simpleName
+        return annotation?.value ?: objClass.simpleName
     }
 }
