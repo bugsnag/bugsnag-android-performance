819512f1c75ca076da949d7a58788d5b17e373d2
SB-sachinp <sachin.patil@smartbear.com>
2026-06-30 21:07:55 +0530

    Buildkite fix

--- features/fixtures/mazeracer/app/src/main/java/com/bugsnag/mazeracer/scenarios/AppSessionResourceUsageScenario.kt
+++ features/fixtures/mazeracer/app/src/main/java/com/bugsnag/mazeracer/scenarios/AppSessionResourceUsageScenario.kt
@@ -1,7 +1,6 @@
 package com.bugsnag.mazeracer.scenarios
 
 import com.bugsnag.android.performance.BugsnagPerformance
-import com.bugsnag.android.performance.AppSessionConfig
 import com.bugsnag.android.performance.PerformanceConfiguration
 import com.bugsnag.android.performance.internal.InternalDebug
 import com.bugsnag.mazeracer.Scenario
